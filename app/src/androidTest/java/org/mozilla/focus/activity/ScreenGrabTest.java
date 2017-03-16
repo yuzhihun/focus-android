20699ceabca58656e0da7d754960c32e1a4ad3c2
No-Jun Park <npark@mozilla.com>
2017-03-16 15:29:48 -0400

    add all tests

--- app/src/androidTest/java/org/mozilla/focus/activity/ScreenGrabTest.java
+++ app/src/androidTest/java/org/mozilla/focus/activity/ScreenGrabTest.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.preference.PreferenceManager;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.IdlingPolicies;
-import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -18,7 +16,6 @@ import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
 import android.support.test.uiautomator.Until;
 import android.text.format.DateUtils;
-import android.webkit.WebView;
 
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -26,16 +23,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mozilla.focus.R;
 
-import java.util.concurrent.TimeUnit;
-
 import tools.fastlane.screengrab.Screengrab;
 import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
 import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.pressKey;
-import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
@@ -65,19 +58,15 @@ public class ScreenGrabTest {
             PreferenceManager.getDefaultSharedPreferences(appContext)
                     .edit()
                     .putBoolean(FIRSTRUN_PREF, false)
-            //        .putBoolean(resources.getString(R.string.pref_key_secure), false)
                     .apply();
-
         }
     };
-
-    private IdlingResource mIdlingResource;
 
     public static void swipeDownNotificationBar (UiDevice deviceInstance) {
         int dHeight = deviceInstance.getDisplayHeight();
         int dWidth = deviceInstance.getDisplayWidth();
         int xScrollPosition = dWidth/2;
-        int yScrollStop = dHeight/3;
+        int yScrollStop = dHeight/4 * 3;
         deviceInstance.swipe(
                 xScrollPosition,
                 yScrollStop,
@@ -91,72 +80,71 @@ public class ScreenGrabTest {
     public void screenGrabTest() throws InterruptedException, UiObjectNotFoundException {
         UiDevice mDevice;
         final long waitingTime = DateUtils.SECOND_IN_MILLIS * 5;
-        IdlingPolicies.setMasterPolicyTimeout(waitingTime * 2, TimeUnit.MILLISECONDS);
-        IdlingPolicies.setIdlingResourceTimeout(waitingTime * 2, TimeUnit.MILLISECONDS);
 
         // Initialize UiDevice instance
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
 
         /* Wait for app to load, and take the First View screenshot */
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.firstrun_exitbutton), isDisplayed()));
-        Screengrab.screenshot("First_View");
+        UiObject firstViewBtn = mDevice.findObject(new UiSelector()
+        .resourceId("org.mozilla.focus.debug:id/firstrun_exitbutton")
+        .enabled(true));
+        firstViewBtn.waitForExists(waitingTime);
+        Screengrab.screenshot("FirstUI_View");
 
         /* Home View*/
-        appCompatButton.perform(click());
-        BySelector urlbar = By.clazz("android.widget.TextView")
-                .res("org.mozilla.focus.debug","url")
-                .clickable(true);
-        mDevice.wait(Until.hasObject(urlbar), waitingTime);
+        firstViewBtn.click();
+        UiObject urlBar = mDevice.findObject(new UiSelector()
+                .resourceId("org.mozilla.focus.debug:id/url")
+                .clickable(true));
+        urlBar.waitForExists(waitingTime);
         Screengrab.screenshot("Home_View");
 
         /* Main View Menu */
-        ViewInteraction appCompatImageButton = onView(
+        ViewInteraction menuButton = onView(
                 allOf(withId(R.id.menu),
                         isDisplayed()));
+        menuButton.perform(click());
 
-        appCompatImageButton.perform(click());
-        Screengrab.screenshot("MainViewMenu");
-
-        /* Your Rights Page */
         UiObject RightsItem = mDevice.findObject(new UiSelector()
                 .className("android.widget.LinearLayout")
                 .instance(2));
+        RightsItem.waitForExists(waitingTime);
+        Screengrab.screenshot("MainViewMenu");
+
+        /* Your Rights Page */
         RightsItem.click();
-        BySelector RightsWebView = By.clazz("android.webkit.Webview")
-                .res("org.mozilla.focus.debug","webview")
-                .focused(true)
-                .enabled(true);
-        mDevice.wait(Until.hasObject(RightsWebView),waitingTime);
+        UiObject webView = mDevice.findObject(new UiSelector()
+        .className("android.webkit.Webview")
+        .focused(true)
+        .enabled(true));
+        webView.waitForExists(waitingTime);
         Screengrab.screenshot("YourRights_Page");
 
         /* About Page */
         mDevice.pressBack();
-        appCompatImageButton.perform(click());
+        menuButton.perform(click());
         UiObject AboutItem = mDevice.findObject(new UiSelector()
                 .className("android.widget.LinearLayout")
                 .instance(0)
                 .enabled(true));
         AboutItem.click();
-        BySelector AboutWebView = By.clazz("android.webkit.Webview")
-                .res("org.mozilla.focus.debug","webview")
-                .focused(true)
-                .enabled(true);
-        mDevice.wait(Until.hasObject(AboutWebView),waitingTime);
+        webView.waitForExists(waitingTime);
         Screengrab.screenshot("About_Page");
 
         /* Location Bar View */
         mDevice.pressBack();
-        ViewInteraction LocationBar = onView(
-                allOf(withId(R.id.url), isDisplayed()));
-        LocationBar.perform(click());
+        urlBar.click();
+        UiObject inlineAutocompleteEditText = mDevice.findObject(new UiSelector()
+                .resourceId("org.mozilla.focus.debug:id/url_edit")
+                .focused(true)
+                .enabled(true));
+        inlineAutocompleteEditText.waitForExists(waitingTime);
         Screengrab.screenshot("LocationBarEmptyState");
 
         /* Autocomplete View */
-        ViewInteraction inlineAutocompleteEditText = onView(
-                allOf(withId(R.id.url_edit), isDisplayed()));
-        inlineAutocompleteEditText.perform(replaceText("mozilla"));
+        inlineAutocompleteEditText.clearTextField();
+        inlineAutocompleteEditText.setText("mozilla");
         BySelector hint = By.clazz("android.widget.TextView")
                 .res("org.mozilla.focus.debug","search_hint")
                 .clickable(true);
@@ -164,13 +152,9 @@ public class ScreenGrabTest {
         Screengrab.screenshot("SearchFor");
 
         /* Browser View Menu */
-        ViewInteraction submitURL = onView(
-                allOf(withId(R.id.url_edit), isDisplayed()));
-        submitURL.perform(pressKey(KEYCODE_ENTER));
-        mDevice.wait(Until.findObject(By.clazz(WebView.class)), waitingTime);
-        ViewInteraction BrowserViewMenuButton = onView(
-                allOf(withId(R.id.menu), isDisplayed()));
-        BrowserViewMenuButton.perform(click());
+        mDevice.pressKeyCode(KEYCODE_ENTER);
+        webView.waitForExists(waitingTime);
+        menuButton.perform(click());
         Screengrab.screenshot("BrowserViewMenu");
 
         /* History Erase Notification */
@@ -185,7 +169,7 @@ public class ScreenGrabTest {
         Screengrab.screenshot("YourBrowingHistoryHasBeenErased");
 
         /* Take Settings View */
-        appCompatImageButton.perform(click());
+        menuButton.perform(click());
         UiObject appItem = mDevice.findObject(new UiSelector()
                 .className("android.widget.LinearLayout")
                 .instance(3));
